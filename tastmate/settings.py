2f711b60c4c9c7f78481335904edd54de698c93f
Dao Duy Anh <duyanhbmehust@gmail.com>
2021-08-10 16:20:04 +0200

    staticfiles added

--- tastmate/settings.py
+++ tastmate/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'tastmate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.postgresql',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
